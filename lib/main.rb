da64631d3f2dc012ab0765a0f986f7efb6adb85a
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-10-17 15:41:09 +0900

    using gen-ai gem

--- lib/main.rb
+++ lib/main.rb
@@ -1,24 +1,6 @@
 # frozen_string_literal: true
 
-require 'dotenv/load'
-require 'gemini-ai'
+require 'gen_ai'
 
-# With an API key
-client = Gemini.new(
-  credentials: {
-    service: 'generative-language-api',
-    api_key: ENV['GOOGLE_GEMINI_TOKEN']
-  },
-  options: { model: 'gemini-pro', server_sent_events: true }
-)
-
-result = client.stream_generate_content({
-  contents: { role: 'user', parts: { text: '日報例文を書いてください' } }
-})
-
-mail_template = result
-              .map { |response| response.dig('candidates', 0, 'content', 'parts') }
-              .map { |parts| parts.map { |part| part['text'] }.join }
-              .join
-
-puts mail_template
+model = GenAI::Language.new(:gemini, ENV['GOOGLE_GEMINI_TOKEN'])
+model.chat("日報例文を書いてください")
