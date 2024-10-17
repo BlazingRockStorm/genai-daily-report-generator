11aed40f0b755fab5d9740a6bfcd18fb4acb4372
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-10-17 15:43:24 +0900

    refactor code

--- Gemfile
+++ Gemfile
@@ -2,6 +2,5 @@
 
 source 'https://rubygems.org'
 
-# gem 'gen-ai', '~> 0.4.2'
 gem 'gemini-ai', '~> 4.2.0'
 gem 'dotenv'
--- main_v1.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'gen_ai'
-
-model = GenAI::Language.new(:gemini, ENV['GOOGLE_GEMINI_TOKEN'])
-model.chat("日報例文を書いてください")
