d56dfefffdffb76f7d425d47b5a9a3ebf37588c9
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-10-17 18:22:31 +0900

    using files as inputs for report

--- lib/main.rb
+++ lib/main.rb
@@ -12,8 +12,35 @@ client = Gemini.new(
   options: { model: 'gemini-pro', server_sent_events: true }
 )
 
+text_request = <<-TEXT
+この日報フォーマットに情報を追加てください。
+
+"A様
+
+お疲れ様です。ホアンクアンです。
+
+本日の業務内容を報告していたします。
+
+業務内容：
+  1. プロジェクト１
+    - タスク１：
+      進捗状況: %
+
+課題
+
+明日の予定
+  1. プロジェクト１
+    プロジェクト１の予定
+
+以上、本日の報告とさせていただきます。"
+TEXT
+
+Dir.glob('./input/*.txt').each do |file_name|
+  text_request += File.read(file_name)
+end
+
 result = client.stream_generate_content({
-  contents: { role: 'user', parts: { text: '日報例文を書いてください' } }
+  contents: { role: 'user', parts: { text: text_request } }
 })
 
 mail_template = result
